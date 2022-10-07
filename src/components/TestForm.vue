e546061e596d93f816c45d042da33be4f77ab278
Jeenyhus <mweembadabwitso@gmail.com>
2022-10-07 07:39:58 +0200

    'components'

--- src/components/TestForm.vue
+++ src/components/TestForm.vue
@@ -2,7 +2,7 @@
   <form id="test-form" @submit.prevent="handleSubmit">
     <div id="header">
         <img src="/images/edulution_logo.png" alt="edulution logo"> <br> <br> <br>
-        <h1>Level 1</h1>
+        <h1>Sample Test 1</h1>
         <h2>Section 1</h2>
     </div>
     <div class="learner-section">
@@ -32,6 +32,74 @@
         <p>
             <input type="radio" id="q1_d" name="q1" ng-model="testResponse" value="1">
             <label for="q1_d">3</label>
+        </p>
+        <h4>(2) 2 &times; 4 &equals;</h4>
+        <p>
+            <input type="radio" id="q2_a" name="q2" ng-model="testResponse" value="1">
+            <label for="q2_a">8</label>
+        </p>
+        <p>
+            <input type="radio" id="q2_b" name="q2" ng-model="testResponse" value="00">
+            <label for="q2_b">9</label>
+        </p>
+        <p>
+            <input type="radio" id="q2_c" name="q2" ng-model="testResponse" value="000">
+            <label for="q2_c">16</label>
+        </p>
+        <p>
+            <input type="radio" id="q2_d" name="q2" ng-model="testResponse" value="0000">
+            <label for="q2_d">24</label>
+        </p>
+        <h4>(3) 2 &plus; 18 &equals;</h4>
+        <p>
+            <input type="radio" id="q3_a" name="q3" ng-model="testResponse" value="0">
+            <label for="q3_a">34</label>
+        </p>
+        <p>
+            <input type="radio" id="q3_b" name="q3" ng-model="testResponse" value="00">
+            <label for="q3_b">19</label>
+        </p>
+        <p>
+            <input type="radio" id="q3_c" name="q3" ng-model="testResponse" value="1">
+            <label for="q3_c">20</label>
+        </p>
+        <p>
+            <input type="radio" id="q3_d" name="q3" ng-model="testResponse" value="0000">
+            <label for="q3_d">36</label>
+        </p>
+        <h4>(4) 30 &minus; 10 &equals;</h4>
+        <p>
+            <input type="radio" id="q4_a" name="q4" ng-model="testResponse" value="0">
+            <label for="q4_a">34</label>
+        </p>
+        <p>
+            <input type="radio" id="q4_b" name="q4" ng-model="testResponse" value="00">
+            <label for="q4_b">19</label>
+        </p>
+        <p>
+            <input type="radio" id="q4_c" name="q4" ng-model="testResponse" value="1">
+            <label for="q4_c">20</label>
+        </p>
+        <p>
+            <input type="radio" id="q4_d" name="q4" ng-model="testResponse" value="0000">
+            <label for="q4_d">36</label>
+        </p>
+        <h4>(5) 10 &times 10 &equals;</h4>
+        <p>
+            <input type="radio" id="q5_a" name="q5" ng-model="testResponse" value="0">
+            <label for="q5_a">10</label>
+        </p>
+        <p>
+            <input type="radio" id="q5_b" name="q5" ng-model="testResponse" value="1">
+            <label for="q5_b">100</label>
+        </p>
+        <p>
+            <input type="radio" id="q5_c" name="q5" ng-model="testResponse" value="000">
+            <label for="q5_c">50</label>
+        </p>
+        <p>
+            <input type="radio" id="q5_d" name="q5" ng-model="testResponse" value="0000">
+            <label for="q5_d">1000</label>
         </p>
     </div>
     <div class="coach-sections">
@@ -75,6 +143,7 @@ export default {
 </script>
 
 <style>
+    
 body {
     background-color: rgb(179, 255, 179);
     font-family: Roboto, RobotoDraft, Helvetica, Arial, sans-serif;
@@ -182,8 +251,8 @@ input {
     content: '';
     width: 11px;
     height: 11px;
-    background: #008000;
-    border: 1px solid #008000;
+    background: #0caf0c;
+    border: 1px solid #0caf0c;
     position: absolute;
     top: 3px;
     left: 3px;
